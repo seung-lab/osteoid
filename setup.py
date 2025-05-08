57b68a5cc7d03c85da40d3db91d28ea017439abc
William Silversmith <william.silversmith@gmail.com>
2025-05-08 17:47:43 -0400

    install: add setup.py for building fastosteoid

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import setuptools
+import sys
+import platform
+
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++17', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++17', '-O3',
+  ]
+
+setuptools.setup(
+  setup_requires=['pbr','pybind11','numpy'],
+  cmdclass={"build_ext": build_ext},
+  version="0.0.1",
+  ext_modules=[
+    Pybind11Extension(
+        "fastosteoid",
+        ["src/fastosteoid.cpp"],
+        extra_compile_args=extra_compile_args,
+        language="c++",
+    ),
+  ]
+)
