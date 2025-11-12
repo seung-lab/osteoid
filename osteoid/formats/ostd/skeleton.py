79db86a2b241e0c6c4cafa47bf3b8319db4d93fd
William Silversmith <william.silversmith@gmail.com>
2025-11-12 16:10:41 -0500

    feat: add setter for properties

--- osteoid/formats/ostd/skeleton.py
+++ osteoid/formats/ostd/skeleton.py
@@ -190,6 +190,9 @@ class OstdSkeletonProperties:
       return val()
     return val
 
+  def __setattr__(self, key:str, val:Any):
+    self._props[key] = val
+
 # represents a full skeleton including
 # multiple parts
 class OstdSkeleton:
