c9549ebfb888843440d52001bd019d125db3c5b6
William Silversmith <william.silversmith@gmail.com>
2025-11-14 14:41:06 -0500

    feat: add more sophisticated load/save

--- osteoid/util.py
+++ osteoid/util.py
@@ -1,23 +1,73 @@
+from typing import Union, IO
+
+import mmap
+import os
+import gzip
+import lzma
+
 from .skeleton import Skeleton
 
-def load(filename:str) -> Skeleton:
-	with open(filename, "rb") as f:
-		data = f.read()
+def _load(filelike, size:int = -1, allow_mmap:bool = False) -> IO[bytes]:
+  if hasattr(filelike, 'read'):
+    binary = filelike.read(size)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] == '.gz'
+  ):
+    with gzip.open(filelike, 'rb') as f:
+      binary = f.read(size)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] in ('.lzma', '.xz')
+  ):
+    with lzma.open(filelike, 'rb') as f:
+      binary = f.read(size)
+  else:
+    with open(filelike, 'rb') as f:
+      if allow_mmap:
+        binary = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+      else:
+        binary = f.read(size)
+  
+  return binary
 
-	if filename.endswith("swc"):
-		return Skeleton.from_swc(data.decode("utf8"))
-	elif filename.endswith("ostd"):
-		return Skeleton.from_ostd(data)
-	else:
-		return Skeleton.from_precomputed(data)
+def load(filename:str, allow_mmap:bool = False) -> Skeleton:
+  binary = _load(filename, allow_mmap=mmap)
 
-def save(filename:str, skel:Skeleton):
-	if filename.endswith("swc"):
-		binary = skel.to_swc()
-	elif filename.endswith("ostd"):
-		binary = skel.to_ostd()
-	else:
-		binary = skel.to_precomputed()
+  if filename.endswith("swc"):
+    return Skeleton.from_swc(binary.decode("utf8"))
+  elif filename.endswith("ostd"):
+    return Skeleton.from_ostd(binary)
+  else:
+    return Skeleton.from_precomputed(binary)
 
-	with open(filename, "wb") as f:
-		f.write(binary)
+def save(
+  filelike,
+  skeleton:Skeleton,
+  **kwargs
+):
+  """Save labels into the file-like object or file path."""
+  if filename.endswith("swc"):
+    binary = skel.to_swc()
+  elif filename.endswith("ostd"):
+    binary = skel.to_ostd()
+  else:
+    binary = skel.to_precomputed()
+
+  if hasattr(filelike, 'write'):
+    filelike.write(binary)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] == '.gz'
+  ):
+    with gzip.open(filelike, 'wb') as f:
+      f.write(binary)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] in ('.lzma', '.xz')
+  ):
+    with lzma.open(filelike, 'wb') as f:
+      f.write(binary)
+  else:
+    with open(filelike, 'wb') as f:
+      f.write(binary)
