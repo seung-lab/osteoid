a86a09652f1acf9b010f8957a5d18fe08a62f740
William Silversmith <william.silversmith@gmail.com>
2025-11-14 14:08:52 -0500

    perf: use smallest dtype to serialize edges

--- osteoid/lib.py
+++ osteoid/lib.py
@@ -9,6 +9,27 @@ import google_crc32c
 
 import numpy as np
 
+def compute_byte_width(x:int) -> int:
+  byte_width = 8
+  if x <= np.iinfo(np.uint8).max:
+    byte_width = 1
+  elif x <= np.iinfo(np.uint16).max:
+    byte_width = 2
+  elif x <= np.iinfo(np.uint32).max:
+    byte_width = 4
+
+  return byte_width
+
+width2dtype = {
+  1: np.uint8,
+  2: np.uint16,
+  4: np.uint32,
+  8: np.uint64,
+}
+
+def compute_dtype(x:int) -> np.dtype:
+  return width2dtype[compute_byte_width(x)]
+
 # selected from https://users.ece.cmu.edu/~koopman/crc/index.html
 # based on a header that is larger than what a crc8 can handle
 def crc16(data:bytes) -> int:
