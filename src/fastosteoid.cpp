e6be15603922d018a924f3d7dcf9c9d0d3202492
William Silversmith <william.silversmith@gmail.com>
2025-11-14 18:32:52 -0500

    feat: add linked_paths calculator

--- src/fastosteoid.cpp
+++ src/fastosteoid.cpp
@@ -13,36 +13,245 @@
 
 namespace py = pybind11;
 
-// uint64_t c_eytzinger_binary_search(uint64_t x, uint64_t* array, size_t N) {
-//     // int64_t block_size = 8; // two cache lines 64 * 2 / 8
-//     uint64_t k = 1;
-//     while (k <= (uint64_t)N) {
-//         // __builtin_prefetch(array + k * block_size);
-//         // multiply by 2 b/c index is [label, pos, label, pos]
-//         k = 2 * k + (array[(k - 1) << 1] < x); 
-//     }
-//     k >>= mb_ffs(~k);
-//     k -= 1;
+template <typename EDGE_T>
+py::list paths_to_pylist(const std::vector<std::vector<EDGE_T>>& paths) {
+    py::list out(paths.size());
 
-//     if (k >= 0 && array[k << 1] == x) {
-//         return k;
-//     }
+    for (size_t i = 0; i < paths.size(); i++) {
+        const auto& p = paths[i];
+        py::array_t<EDGE_T> arr(p.size());
+        auto* dst = static_cast<EDGE_T*>(arr.mutable_data());
+        std::memcpy(dst, p.data(), p.size() * sizeof(EDGE_T));
+        out[i] = arr;
+    }
 
-//     return -1;
-// }
+    return out;
+}
 
-// uint32_t c_eytzinger_sort_indices(
-//     uint32_t* array, uint32_t n, 
-//     uint32_t i /*=0*/, uint32_t k /*=1*/
-// ) {
-//     if (k <= n) {
-//         i = c_eytzinger_sort_indices(array, n, i, 2 * k);
-//         array[k-1] = i;
-//         i++;
-//         i = c_eytzinger_sort_indices(array, n, i, 2 * k + 1);
-//     }
-//     return i;
-// }
+template <typename T>
+py::array_t<T> pairs_to_numpy(
+	const std::vector<std::pair<T, T>>& pairs
+) {
+	size_t n = pairs.size();
+	py::array_t<T> arr({n, size_t(2)});
+	auto buf = arr.template mutable_unchecked<2>();
+
+	for (size_t i = 0; i < n; ++i) {
+			buf(i, 0) = pairs[i].first;
+			buf(i, 1) = pairs[i].second;
+	}
+
+	return arr;
+}
+
+template <typename T>
+std::vector<std::pair<T,T>> unique(
+	std::vector<std::pair<T,T>>& input
+) {
+	if (input.size() == 0) {
+		return std::vector<std::pair<T,T>>();
+	}
+
+	std::sort(input.begin(), input.end());
+	
+	const uint64_t N = input.size();
+
+	std::vector<std::pair<T,T>> uniq;
+	uniq.reserve(N / 10);
+
+	uniq.push_back(input[0]);
+	
+	for (uint64_t i = 1; i < N; i++) {
+		if (input[i] != input[i-1]) {
+			uniq.push_back(input[i]);
+		}
+	}
+
+	return uniq;
+}
+
+template <typename EDGE_T>
+py::tuple linked_paths_impl(const py::array_t<EDGE_T>& edges_arr) {
+	auto buf = edges_arr.request();
+	if (buf.ndim != 2 || buf.shape[1] != 2) {
+		throw std::runtime_error("edges must be an (N,2) array");
+	}
+	const uint64_t Ne = edges_arr.size() >> 1;
+	EDGE_T* edges = static_cast<EDGE_T*>(buf.ptr);
+
+
+	uint64_t Nv = 0;
+	for (size_t i = 0; i < 2 * Ne; i++) {
+		Nv = std::max(Nv, static_cast<uint64_t>(edges[i]));
+	}
+	Nv++;
+
+	std::vector<bool> visited(Nv);
+	std::vector< ankerl::unordered_dense::set<EDGE_T> > index(Nv);
+
+	for (size_t i = 0; i < 2 * Ne; i += 2) {
+		EDGE_T e1 = edges[i];
+		EDGE_T e2 = edges[i+1];
+
+		index[e1].insert(e2);
+		index[e2].insert(e1);
+	}
+
+	auto extract_branches = [&](EDGE_T start) -> py::array_t<EDGE_T> {
+		std::vector<EDGE_T> stack;
+		std::vector<EDGE_T> parents;
+
+		std::vector<std::pair<EDGE_T, EDGE_T>> edge_list;
+		std::vector<std::vector<EDGE_T>> paths;
+ 		std::vector<EDGE_T> path;
+
+ 		path.push_back(start);
+
+		visited[start] = true;
+		for (EDGE_T child : index[start]) {
+			stack.push_back(child);
+			parents.push_back(start);
+		}
+
+		bool cycle_detected = false;
+
+		while (stack.size()) {
+			EDGE_T node = stack.back();
+			EDGE_T parent = parents.back();
+
+			stack.pop_back();
+			parents.pop_back();
+
+			if (node == parent) {
+				continue;
+			}
+
+ 			path.push_back(node);
+
+			// check visited after because you can visit a node 
+			// multiple times for different parents, but you don't
+			// want to keep re-adding it to the stack.
+			if (visited[node]) {
+				continue;
+			}
+
+			while (true) {
+				visited[node] = true;
+				
+				bool picked = false;
+				EDGE_T next = 0;
+				
+				for (EDGE_T child : index[node]) {
+					if (child == parent) {
+						continue;
+					}
+					else if (visited[child]) {
+						cycle_detected = true;
+						continue;
+					}
+					else if (picked) {
+						if (node < child) {
+							edge_list.emplace_back(node, child);
+						}
+						else {
+							edge_list.emplace_back(child, node);
+						}
+						stack.push_back(child);
+						parents.push_back(node);
+					}
+					else {
+						picked = true;
+						next = child;
+						path.push_back(child);
+					}
+				}
+
+				if (picked) {
+					parent = node;
+					node = next;
+				}
+				else {
+					break;
+				}
+			}
+
+			paths.push_back(path);
+			path.clear();
+		}
+
+		auto uniq = unique<EDGE_T>(edge_list);
+		py::tuple out(3);
+		out[0] = paths_to_pylist<EDGE_T>(paths);
+		out[1] = pairs_to_numpy<EDGE_T>(uniq);
+		out[2] = cycle_detected;
+
+		return out;
+	};
+
+	py::tuple ret(4);
+
+	py::list all_paths;
+	py::list all_edges;
+
+	uint64_t N = 0;
+	bool has_cycle = false;
+
+	for (size_t i = 0; i < Ne * 2; i += 2) {
+		EDGE_T edge = edges[i];
+
+		if (visited[edge]) {
+			continue;
+		}
+
+		py::tuple tup = extract_branches(edge);
+		all_paths.append(tup[0]);
+		all_edges.append(tup[1]);
+		has_cycle |= tup[2].cast<bool>();;
+		N++;
+	}
+
+	ret[0] = all_paths;
+	ret[1] = all_edges;
+	ret[2] = has_cycle;
+	ret[3] = N;
+
+	return ret;
+}
+
+py::tuple linked_paths(const py::array &edges_arr) {
+	py::buffer_info buf = edges_arr.request();
+
+	if (buf.ndim != 2 || buf.shape[1] != 2) {
+		throw std::runtime_error("edges must be an (N,2) array");
+	}
+
+	int data_width = edges_arr.dtype().itemsize();
+
+	if (data_width == 1) {
+		if (buf.strides[1] != sizeof(uint8_t)) {
+			throw std::runtime_error("Array must be C-contiguous");
+		}
+		return linked_paths_impl<uint8_t>(edges_arr);
+	}
+	else if (data_width == 2) {
+		if (buf.strides[1] != sizeof(uint16_t)) {
+			throw std::runtime_error("Array must be C-contiguous");
+		}
+		return linked_paths_impl<uint16_t>(edges_arr);
+	}
+	else if (data_width == 4) {
+		if (buf.strides[1] != sizeof(uint32_t)) {
+			throw std::runtime_error("Array must be C-contiguous");
+		}
+		return linked_paths_impl<uint32_t>(edges_arr);
+	}
+	else {
+		if (buf.strides[1] != sizeof(uint64_t)) {
+			throw std::runtime_error("Array must be C-contiguous");
+		}
+		return linked_paths_impl<uint64_t>(edges_arr);
+	}
+}
 
 // Ne = size of edges / 2
 // Nv = number of vertices (max of edge values)
@@ -158,50 +367,6 @@ bool has_cycle(const py::array& edges) {
 	}
 }
 
-template <typename T>
-py::array_t<T> pairs_to_numpy(
-	const std::vector<std::pair<T, T>>& pairs
-) {
-		size_t n = pairs.size();
-
-		// Create an uninitialized NumPy array of shape (n, 2)
-		py::array_t<T> arr({n, size_t(2)});
-		auto buf = arr.template mutable_unchecked<2>();
-
-		for (size_t i = 0; i < n; ++i) {
-				buf(i, 0) = pairs[i].first;
-				buf(i, 1) = pairs[i].second;
-		}
-
-		return arr;
-}
-
-template <typename T>
-std::vector<std::pair<T,T>> unique(
-	std::vector<std::pair<T,T>>& input
-) {
-	if (input.size() == 0) {
-		return std::vector<std::pair<T,T>>();
-	}
-
-	std::sort(input.begin(), input.end());
-	
-	const uint64_t N = input.size();
-
-	std::vector<std::pair<T,T>> uniq;
-	uniq.reserve(N / 10);
-
-	uniq.push_back(input[0]);
-	
-	for (uint64_t i = 1; i < N; i++) {
-		if (input[i] != input[i-1]) {
-			uniq.push_back(input[i]);
-		}
-	}
-
-	return uniq;
-}
-
 template <typename EDGE_T>
 py::list compute_components_impl(
 	const py::array_t<EDGE_T> &edges_arr,
@@ -296,7 +461,7 @@ py::list compute_components_impl(
 	}
 
 	return forest;
-}
+}	
 
 py::list compute_components(
 	const py::array &edges_arr,
@@ -338,6 +503,7 @@ py::list compute_components(
 
 PYBIND11_MODULE(fastosteoid, m) {
 	m.doc() = "Accelerated osteoid functions."; 
+	m.def("linked_paths", &linked_paths, "Compute the linked paths edge representation.");
 	m.def("has_cycle", &has_cycle, "Check whether this connected component has a cycle.");
 	m.def("compute_components", &compute_components, "Find skeleton components.");
 }
